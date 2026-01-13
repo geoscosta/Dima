5dcad7e1d0c014187c3d4c308b55ce68018f15ce
geoscosta <geoscosta@gmail.com>
2026-01-13 13:25:14 -0300

    Configurando o Identity Api no Program

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +27,12 @@ builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(connection);
 });
+
+builder.Services
+    .AddIdentityCore<User>()
+    .AddRoles<IdentityRole<long>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
