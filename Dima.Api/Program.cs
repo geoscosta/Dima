c912c493ea1d84b7b250631e0fadc1a62bb8070f
geoscosta <geoscosta@gmail.com>
2026-01-13 14:18:51 -0300

    Implementando endpoint logout e roles

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
@@ -46,5 +47,39 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapEndpoints();
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapIdentityApi<User>();
 
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", async (SignInManager<User> signInManager) =>
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    })
+    .RequireAuthorization();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles", (ClaimsPrincipal user) =>
+    {
+        if(user.Identity is null || !user.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+        
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity.FindAll(identity.RoleClaimType)
+            .Select(c => new
+            {
+                c.Issuer,
+                c.OriginalIssuer,
+                c.Type,
+                c.Value,
+                c.ValueType
+            });
+
+        return TypedResults.Json(roles);
+    })
+    .RequireAuthorization();
+    
 app.Run();
--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dcad7e1d0c014187c3d4c308b55ce68018f15ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beba509853e44e9316c728a7d3a2f181f7611ff8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
